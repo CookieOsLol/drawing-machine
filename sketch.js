7f5f36a35e6716883113424626c174d8d3e0a248
CookieOsLol <antonybui408@gmail.com>
2021-04-13 23:48:17 -0700

    click

--- sketch.js
+++ sketch.js
@@ -5,28 +5,31 @@ let img2;
 let img3;
 let img4;
 let recoil;
+let cnv;
+
 
 function preload(){
 img = loadImage('assets/bh1.png');
 img2 = loadImage('assets/bh2.png');
 img3 = loadImage('assets/bh3.png');
 img4 = loadImage('assets/td.png');
-recoil = random(40,150);
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background(154,130,64);
 image(img4,400,10,1000,1000);
-
+recoil = random(40,150);
 }
 
 function draw() {
+  cnv.mouseClicked(mouseClicked)
 
+}
 
-if (mouseIsPressed){
+function mouseClicked(){
 //image(img2,mouseX-60,mouseY-50,150,150);
-image(img2,mouseX -(recoil),mouseY-(recoil),150,150);
+image(img2,mouseX -60,mouseY-50,150,150);
 
   //opposite ->    //line(width -mouseX, height -mouseY,width -pmouseX,height-pmouseY);
   //background(220,50,133,5);
@@ -37,7 +40,7 @@ image(img2,mouseX -(recoil),mouseY-(recoil),150,150);
 //stroke(map(mouseX, 0, 600, 0, 255,true))
 //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
 //line(mouseX, mouseY, pmouseX, pmouseY);
-}
+
 
 }
 
