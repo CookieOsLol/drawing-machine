afa5adc4bf7445c93934723f95e55ffbb1d26db1
CookieOsLol <antonybui408@gmail.com>
2021-04-14 03:03:28 -0700

    i tried

--- sketch.js
+++ sketch.js
@@ -7,32 +7,45 @@ let img4;
 let recoil;
 let cnv;
 let mySound;
+let i = 0;
+let x = 0;
 
 function preload(){
 img4 = loadImage('assets/bh1.png');
-img2 = loadImage('assets/bh2.png');
+img2 = loadImage('assets/white.png');
 img3 = loadImage('assets/bh3.png');
 img = loadImage('assets/td.png');
 soundFormats('mp3','ogg');
 mySound = loadSound('assets/gun');
-recoil = 60
+recoil = 60;
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background(154,130,64);
 image(img,400,10,1000,1000);
+flash1 = new flash(0);
+
 }
 
 function draw() {
-  cnv.mouseClicked(mouseClicked)
+x = 0;
+squareColor = color(255);
+squareColor.setAlpha(x);
+noStroke();
+fill(squareColor);
+rect(0,0,10000,10000);
+
+
+  cnv.mouseClicked(mouseClicked);
 
 }
 
 function mouseClicked(){
 //image(img2,mouseX-60,mouseY-50,150,150);
-image(img4,mouseX -(recoil+(random(-5,10))),mouseY-(recoil+(random(-5,10))),170,170);
+image(img3,mouseX -(recoil+(random(-5,10))),mouseY-(recoil+(random(-5,10))),120,120);
 mySound.play();
+
   //opposite ->    //line(width -mouseX, height -mouseY,width -pmouseX,height-pmouseY);
   //background(220,50,133,5);
 //strokeWeight(strokeWidth);
@@ -43,6 +56,8 @@ mySound.play();
 //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
 //line(mouseX, mouseY, pmouseX, pmouseY);
 }
+
+
 
 
 
